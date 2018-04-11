bda3a62230557801437c29956162caa028b17e98
sharedcare <sharedcare@hotmail.com>
2018-04-10 19:32:16 -0500

    update form in App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,83 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <UploadForm />
       </div>
     );
   }
+}
+
+
+const inputParsers = {
+    date(input) {
+        const [month, day, year] = input.split('/');
+        return `${year}-${month}-${day}`;
+    },
+    uppercase(input) {
+        return input.toUpperCase();
+    },
+    number(input) {
+        return parseFloat(input);
+    },
+};
+
+class UploadForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            contentType: '',
+            imageUrl: '',
+            fileExtension: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    static handleSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        const data = new FormData(form);
+
+        for (let name of data.keys()) {
+            const input = form.elements[name];
+            const parserName = input.dataset.parse;
+
+            if (parserName) {
+                const parser = inputParsers[parserName];
+                const parsedValue = parser(data.get(name));
+                data.set(name, parsedValue);
+            }
+        }
+
+        fetch('https://kir131kf29.execute-api.us-east-2.amazonaws.com/prod/upload-image', {
+            method: 'POST',
+            body: data,
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    data-parse="uppercase"
+                    required
+                />
+
+                <input
+                    name="title"
+                    type="text"
+                    placeholder="Title"
+                    required
+                />
+
+                <button>Upload image!</button>
+            </form>
+        );
+    }
 }
 
 export default App;
